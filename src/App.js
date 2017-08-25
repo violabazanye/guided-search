68e7901bdedd3cce26aed550ae6c58300327e3b0
Viola Bazanye <viola.bazanye@MI-002321-AP.local>
2017-08-25 11:11:20 +0200

    renamed search file

--- src/App.js
+++ src/App.js
@@ -5,17 +5,31 @@ import avatar from './imgs/avatar.png';
 import heart from './imgs/heart.png';
 import bag from './imgs/shopping-bag.png';
 import './App.css';
-import Search from './search.js';
+import { Search } from './Search.js';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {userInput: ''};
+    this.handleUserInput = this.handleUserInput.bind(this);
+  }
+
+  handleUserInput(e){
+    this.setState({
+      userInput: e.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h3>APP NAME</h3>
-          <input type="text" placeholder="Search" />
+          <input type="text" placeholder="Search" onChange={this.handleUserInput} value={this.state.userInput} />
         </div>
         <div className="main-container">
+        <Search />
           <img src={sale} alt="Sale"/>
           <p style={{fontSize: 12}}>FREE SHIPPING FOR ORDERS OVER 30EUR</p>
           <h4 style={{textAlign: 'left', marginLeft: 20}}>OUTFIT OF THE DAY</h4>
